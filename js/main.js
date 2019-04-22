0fa4d49a2127c8aaa3c57bab6e6b1ec54d0b2bc5
JessicaPamela <jpzcode@gmail.com>
2019-04-21 19:48:22 -0500

    functionCalcularMontos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 (function() {
   'use strict';
+
+  var regalo = document.getElementById('regalo');
+
   document.addEventListener('DOMContentLoaded', function() {
 
     //Campo Pases
@@ -18,7 +21,34 @@
     var botonRegistro = document.getElementById('btnRegistro');
     var resultado = document.getElementById('lista-productos');
 
-    
+    //Extras
+    var etiquetas = document.getElementById('etiquetas');
+    var camisas = document.getElementById('camisa_evento');
+
+    calcular.addEventListener('click', calcularMontos);
+
+
+
+    function calcularMontos(event){
+      event.preventDefault();
+      if(regalo.value === '') {
+        alert("Debes elegir un regalo");
+        regalo.focus();
+      } else {
+        var boletosDia = parseInt(pase_dia.value, 10) || 0,
+            boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
+            boletoCompleto = parseInt(pase_completo.value, 10) || 0,
+            cantCamisas = parseInt(camisas.value, 10) || 0,
+            cantEtiquetas = parseInt(etiquetas.value, 10) || 0,;
+
+        var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+        console.log(totalPagar);
+          
+        }
+      }
+
+
+
 
 
   });
