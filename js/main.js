5f261969f8b316664adc6cb56f5fbfc612341f70
JessicaPamela <jpzcode@gmail.com>
2019-04-21 23:23:17 -0500

    TerminandoJsMapa

--- js/main.js
+++ js/main.js
@@ -5,15 +5,16 @@
 
   document.addEventListener('DOMContentLoaded', function() {
 
-    var map = L.map('mapa').setView([51.505, -0.09], 13);
+    var map = L.map('mapa').setView([20.659063, -100.400925], 17);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([20.659063, -100.400925]).addTo(map)
+      .bindPopup('GDLWebCamp 2019 <br> Boletos ya disponibles')
       .openPopup();
+      
 
 
     //Campo Pases
