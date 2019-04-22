2f78b019f05143f6063dbfdebf40c7b6e4c29f1c
JessicaPamela <jpzcode@gmail.com>
2019-04-21 22:06:24 -0500

    terminandoCarrito

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@
     var calcular = document.getElementById('calcular');
     var error = document.getElementById('error');
     var botonRegistro = document.getElementById('btnRegistro');
-    var resultado = document.getElementById('lista-productos');
+    var lista_productos = document.getElementById('lista-productos');
+    var suma = document.getElementById('suma-total');
 
     //Extras
     var etiquetas = document.getElementById('etiquetas');
@@ -39,10 +40,36 @@
             boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
             boletoCompleto = parseInt(pase_completo.value, 10) || 0,
             cantCamisas = parseInt(camisas.value, 10) || 0,
-            cantEtiquetas = parseInt(etiquetas.value, 10) || 0,;
+            cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
 
         var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
-        console.log(totalPagar);
+        
+        var listadoProductos = [];
+
+
+        if (boletosDia >= 1) {
+          listadoProductos.push(boletosDia + ' Pases por día');
+        }
+        if (boletos2Dias >= 1) {
+          listadoProductos.push(boletos2Dias + ' Pases por 2 días');
+        }
+        if (boletoCompleto >= 1) {
+          listadoProductos.push(boletoCompleto + ' Pases completo');
+        }
+        if (cantCamisas >= 1) {
+          listadoProductos.push(cantCamisas + ' Camisas');
+        }
+        if (cantEtiquetas >= 1) {
+          listadoProductos.push(cantEtiquetas + ' Etiquetas');
+        }
+
+        lista_productos.style.display = "block";
+        lista_productos.innerHTML = '';
+        for (var i = 0; i < listadoProductos.length; i++) {
+          lista_productos.innerHTML += listadoProductos[i] + '</br>';
+          
+        }
+        suma.innerHTML = "$ " + totalPagar.toFixed(2); //Dos centavos
           
         }
       }
